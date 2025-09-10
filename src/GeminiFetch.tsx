3b42d90ea59422dbf85831ce3a857fe15b8a126e
papapapsky <nevzlomaezhhaha@gmail.com>
2025-09-10 18:47:10 +0700

    https protocol

--- src/GeminiFetch.tsx
+++ src/GeminiFetch.tsx
@@ -3,7 +3,7 @@ export const geminiFetch = async (
   text: string,
   geminiConfig?: object
 ) => {
-  const AImessage = await fetch("http://31.169.124.125:3000/fetchToGemini", {
+  const AImessage = await fetch("https://31.169.124.125:3000/fetchToGemini", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
